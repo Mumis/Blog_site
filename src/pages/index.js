274020eabb48117e91b1a0fe7e7044227c831492
mumis <emil.wertwein@gmail.com>
2019-08-22 10:50:15 +0200

    fix build error

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from "../components/seo"
-import { Wrapper } from "./indexStyle"
+import { Wrapper } from "../styles/indexStyle"
 
 const IndexPage = (props) => {
   const postList = props.data.allMarkdownRemark;
